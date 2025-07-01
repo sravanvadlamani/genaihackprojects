e36ec57c2e86604067d0664d56497ff282c56e0b
sravanvadlamani <68358851+sravanvadlamani@users.noreply.github.com>
2025-07-01 14:56:40 -0700

    Update mlwithcharts.py

--- mlwithcharts.py
+++ mlwithcharts.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
+import openai
 
 # Load OpenAI key
 # load_dotenv()
