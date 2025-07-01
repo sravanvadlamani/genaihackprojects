c966abe0d6ed3d3e6fa2ea0223bd2c0b98cb623c
sravanvadlamani <68358851+sravanvadlamani@users.noreply.github.com>
2025-07-01 14:50:16 -0700

    Update mlwithcharts.py

--- mlwithcharts.py
+++ mlwithcharts.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import os
-import openai
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
