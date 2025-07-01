b1af7ab524889d6f5d689d028379c1a087601f7e
sravanvadlamani <68358851+sravanvadlamani@users.noreply.github.com>
2025-07-01 14:49:10 -0700

    Update mlwithcharts.py

--- mlwithcharts.py
+++ mlwithcharts.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
-from dotenv import load_dotenv
 import openai
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
